bfd294f4f0fde6c61602863b9f4096d3f404826d
Cocktaille <simon.vandenbergh@gmail.com>
2023-05-30 11:41:23 +0200

    changed header image path

--- src/App.vue
+++ src/App.vue
@@ -22,8 +22,8 @@ const baseStore = useBaseStore()
     <div class="container-fluid text-center">
       
       <div class="full-block mb-4">
-        <img src="@/assets/img/header_festivaltickets.png" alt="" class="d-none d-md-block w-100">
-        <img src="@/assets/img/header_festivaltickets.png" alt="" class="d-md-none w-100">
+        <img src="https://www.nieuwsblad.be/extra/static/2023/202307_festivaltickets/img/header_festivaltickets.png" alt="" class="d-none d-md-block w-100">
+        <img src="https://www.nieuwsblad.be/extra/static/2023/202307_festivaltickets/img/header_festivaltickets.png" alt="" class="d-md-none w-100">
       </div>
 
 
